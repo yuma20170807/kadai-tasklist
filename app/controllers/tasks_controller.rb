e80fc6e9ddc8246c9766811860ca1e8c5880872a
yuma.isono <g031q008@s.iwate-pu.ac.jp>
2020-05-23 08:32:28 +0000

    kadai2 first version

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -41,7 +41,7 @@ class TasksController < ApplicationController
     end
     private
     def task_params
-        params.require(:task).permit(:content)
+        params.require(:task).permit(:content,:status)
     end
 
 end
