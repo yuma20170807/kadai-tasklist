b405dc0efafcbeda4aa5e23eca8cbbb0c278ddbb
yuma.isono <g031q008@s.iwate-pu.ac.jp>
2020-06-02 11:07:13 +0000

    add certification

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,8 @@
 class TasksController < ApplicationController
     before_action :require_user_logged_in ,only: [:index,:show,:new,:edit]
+    before_action :correct_user ,only:[:show,:edit,:update,:destroy]
+    
+    
     def index
         @tasks = current_user.tasks.order(id: :desc)
     end
@@ -44,6 +47,11 @@ class TasksController < ApplicationController
     def task_params
         params.require(:task).permit(:content,:status)
     end
-
+    def correct_user
+        @task = current_user.tasks.find_by(id: params[:id])
+        unless @task
+            flash[:danger]='権限がありません'
+            redirect_to root_url
+        end
+    end
 end
-
