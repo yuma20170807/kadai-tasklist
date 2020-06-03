d7c868da1188aa51b59919549f019237e9422cd3
yuma.isono <g031q008@s.iwate-pu.ac.jp>
2020-06-03 05:42:31 +0000

    Adjustment

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-    before_action :require_user_logged_in ,only: [:index,:show,:new,:edit]
+    before_action :require_user_logged_in
     before_action :correct_user ,only:[:show,:edit,:update,:destroy]
     
     
@@ -7,7 +7,6 @@ class TasksController < ApplicationController
         @tasks = current_user.tasks.order(id: :desc)
     end
     def show
-        @task=Task.find(params[:id])
     end
     def create
         @task=current_user.tasks.build(task_params)
@@ -24,10 +23,8 @@ class TasksController < ApplicationController
         @task=Task.new
     end
     def edit
-        @task=Task.find(params[:id])
     end
     def update
-        @task=Task.find(params[:id])
         if @task.update(task_params)
              flash[:success] = 'Task は正常に更新されました'
              redirect_to @task
@@ -37,7 +34,6 @@ class TasksController < ApplicationController
         end    
     end
     def destroy
-        @task=Task.find(params[:id])
         @task.destroy
         
         flash[:success]='Taskは正常に削除されました'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in ,only: [:show]
-  
-  def show 
-    @user=User.find(params[:id])
-  end
+
   def new
     @user=User.new
   end
